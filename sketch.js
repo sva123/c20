a72011bd8c925de53ce9583b5c22a04385938603
sva123 <76997153+sva123@users.noreply.github.com>
2021-01-20 15:57:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var a,b;
+function setup() {
+  createCanvas(800,400);
+ a=createSprite(400, 200, 50, 50);
+ b=createSprite(300,150, 33, 45);
+ a.shapeColor="yellow";
+ b.shapeColor="gold";
+}
+
+function draw() {
+  background(255,255,255);  
+  a.x=mouseX;
+  a.y=mouseY;
+  console.log(a.x-b.x);
+  drawSprites();
+  
+
+if(a.x-b.x<a.width/2+b.width/2 && b.x-a.x<a.width/2+b.width/2 && a.y-b.y<a.height/2+b.height/2 && b.y-a.y<a.height/2+b.height/2)
+{
+ a.shapeColor="blue";
+ b.shapeColor="blue";
+  }
+  else
+  {  
+    a.shapeColor="yellow";
+    b.shapeColor="gold";
+}
+  
+}
